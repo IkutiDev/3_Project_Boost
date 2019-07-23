8317931f7ea8a2bde065be03103a1b1dfe00a3fd
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-23 15:34:18 +0200

    Added ability to change up a controls a bit.

--- Lunar Miner/Assets/Scripts/Rocket.cs
+++ Lunar Miner/Assets/Scripts/Rocket.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-
+    [SerializeField] private float rcsThrust = 100f;
+    [SerializeField] private float mainThrust = 600f;
     private Rigidbody _rigidBody;
     private AudioSource _audioSource;
     // Start is called before the first frame update
@@ -25,7 +26,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            _rigidBody.AddRelativeForce(Vector3.up);
+            _rigidBody.AddRelativeForce(Vector3.up* mainThrust);
             if (!_audioSource.isPlaying)
             {
                 _audioSource.Play();
@@ -34,20 +35,38 @@ public class Rocket : MonoBehaviour
         else
         {
             _audioSource.Stop();
+            //StartCoroutine(VolumeFade(_audioSource, 0f, soundFadeLength));
         }
     }
     private void Rotate()
     {
 
         _rigidBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
         _rigidBody.freezeRotation = false;
+    }
+
+    private IEnumerator VolumeFade(AudioSource audioSource,float endVolume,float fadeLength)
+    {
+        float startVolume = audioSource.volume;
+        float startTime = Time.time;
+        while (Time.time < startTime+fadeLength)
+        {
+            audioSource.volume = startVolume + (endVolume - startVolume) * ((Time.time - startTime) / fadeLength);
+            yield return null;
+        }
+
+        if (endVolume <= 0)
+        {
+            audioSource.Stop();
+        }
     }
 }
