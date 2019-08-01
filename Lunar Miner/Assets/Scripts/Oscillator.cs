24d1fa4b3eedd691233caf8a956376c1181c7bc6
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-01 21:58:49 +0200

    Level 3 finished, Started work on level 6 and 4. Oscillator upgraded

--- Lunar Miner/Assets/Scripts/Oscillator.cs
+++ Lunar Miner/Assets/Scripts/Oscillator.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
+    enum transformVector
+    {
+        Position,
+        Rotation,
+        Scale
+    }
     [SerializeField] private Vector3 movementVector3 = new Vector3(10,10,10);
-
+    [SerializeField] private transformVector oscillatorVectorType;
     [SerializeField] private float period = 2f;
     // Start is called before the first frame update
     private const float Tau = Mathf.PI * 2; // about 6.28
@@ -16,7 +22,19 @@ public class Oscillator : MonoBehaviour
     private Vector3 startingPos;
     void Start()
     {
-        startingPos = transform.position;
+        switch (oscillatorVectorType)
+        {
+            case transformVector.Position:
+                startingPos = transform.position;
+                break;
+            case transformVector.Rotation:
+                startingPos = new Vector3(transform.rotation.x,transform.rotation.y,transform.rotation.z);
+                break;
+            case transformVector.Scale:
+                startingPos = transform.localScale;
+                break;
+        }
+        
     }
 
     // Update is called once per frame
@@ -30,6 +48,20 @@ public class Oscillator : MonoBehaviour
         Debug.Log(rawSinWave);
         movementFactor = rawSinWave / 2f+0.5f;
         Vector3 offset = movementVector3 * movementFactor;
-        transform.position = startingPos + offset;
+        switch (oscillatorVectorType)
+        {
+            case transformVector.Position:
+                transform.position = startingPos + offset;
+                break;
+            case transformVector.Rotation:
+                Vector3 v3 = startingPos + offset;
+                Quaternion rot = Quaternion.Euler(v3.x,v3.y,v3.z);
+                transform.rotation = rot;
+                break;
+            case transformVector.Scale:
+                transform.localScale= startingPos + offset;
+                break;
+        }
+        
     }
 }
